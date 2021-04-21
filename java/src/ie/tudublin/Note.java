0592ec7c48bcd55ec9c0234cac9df29f15d4d9a7
sibylle221 <sophiesibylle@gmail.com>
2021-04-21 17:10:19 +0100

    Altered note class, added ArrayList

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -1,10 +1,44 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
 public class Note {
 
-    private char note(){
+   private int duration;
+   private char note;
 
-    }
-    
-    private int duration(){
-        
-    }
+//generated constructor
+public Note(int duration, char note) {
+    this.duration = duration;
+    this.note = note;
 }
+
+//generated toString
+@Override
+public String toString() {
+    return "Note [duration=" + duration + ", note=" + note + "]";
+}
+
+//generated getters and setters
+public int getDuration() {
+    return duration;
+}
+
+public void setDuration(int duration) {
+    this.duration = duration;
+}
+
+public char getNote() {
+    return note;
+}
+
+public void setNote(char note) {
+    this.note = note;
+}
+
+
+}//end
+
+
+   
+
