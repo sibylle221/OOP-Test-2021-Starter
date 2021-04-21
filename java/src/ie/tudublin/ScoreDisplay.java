6c5c36183e8c1e2d60f85fe556365cc1e1a8c4e8
sibylle221 <sophiesibylle@gmail.com>
2021-04-21 18:02:15 +0100

    Working on loadScore, trying charAt

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -15,8 +15,10 @@ public class ScoreDisplay extends PApplet
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 	
-	ArrayList<Note> Note = new ArrayList<Note>();
-	
+	ArrayList<Note> Notes = new ArrayList<Note>();
+
+	//schtuff in here later
+
 	public void settings()
 	{
 		size(1000, 500);
@@ -25,6 +27,22 @@ public class ScoreDisplay extends PApplet
 		char c = '7'; // c holds the character 7 (55)
 		int i = c - '0'; // i holds the number 7 (55 - 48) 
 		println(i);
+	}
+
+	//used to populate ArrayList with contents of String score
+	public void loadScore()
+	{
+		for(int j = 0; j < score.length(); j++)
+        {
+            result = score.charAt(j);
+            println(result);
+        }
+	}
+
+	public void printScores()
+	{
+		for (i<) 
+            System.out.println("Number = " + number);
 	}
 
 	public void setup() 
